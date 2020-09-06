f8764003e4bd7b01830323967efe0d20b3bba0a7
Blake Gearin <blakegearin@gmail.com>
2020-09-06 01:44:27 -0500

    Added models & migrations

--- tictactoe/tictactoe/settings.py
+++ tictactoe/tictactoe/settings.py
@@ -18,13 +18,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '@r8z(jk$5go5epg$f$(q^tay(t)+8sy7q&u933s!^vu0*b%#j8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gameplay',
 ]
 
 MIDDLEWARE = [
